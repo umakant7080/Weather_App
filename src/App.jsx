9a1a7a7bc02e5c4423a23c2a054c95d9852f4df0
umakant <umakantkumar7080@gmail.com>
2024-09-25 14:44:31 +0530

    weather app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import  React from 'react'
+import Weather from './components/weather'
+
+const  App =  () => {
+  return(
+    <div  className='app'>
+      <Weather/>
+    </div>
+  )
+}
+
+export default App 
